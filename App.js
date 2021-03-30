d7afe3709f24165fd558f6b8e00a1ffafe01b50d
santiago <santiago.ottolini97@hotmail.com>
2021-03-30 11:09:35 -0300

    React-Native: Image

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World !! COPADO ESTO </Text>
+      <Text style={styles.title}>Hello World</Text>
+      <Image
+       source={{uri:"https://picsum.photos/200/200"}}
+       style={styles.image}
+       />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +21,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ff00ff'
   },
+  title:{
+    fontSize: 24, color: "black"
+  },
+  image:{
+    width:200,
+    height:200
+  }
 });
