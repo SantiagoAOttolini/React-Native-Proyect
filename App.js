f14b99846f6c11f6ff73e0820482680cba1b32f5
santiago <santiago.ottolini97@hotmail.com>
2021-03-30 12:48:58 -0300

    React-Native: Image Picker

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
-import imageDiamond from './assets/diaomond.png'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import imageDiamond from "./assets/diaomond.png";
+import * as ImgPicker from "expo-image-picker";
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImgPicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted == false) {
+      alert("Permision to acces camera is required");
+      return;
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World</Text>
       <Image
-       source={{uri:"https://picsum.photos/200/200"}}
-       /* source={imageDiamond} */
-       style={styles.image}
-       />
-       <Button
-       color="red"
-       title="Add"
-       onPress={() => Alert.alert("Hola")}
-       />
-       <TouchableOpacity
-       style={styles.btn}
-       >
-         <Text style={styles.textBtn}>Hola</Text>
-       </TouchableOpacity>
+        source={{ uri: "https://picsum.photos/200/200" }}
+        /* source={imageDiamond} */
+        style={styles.image}
+      />
+      <Button color="red" title="Add" onPress={openImagePickerAsync} />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textBtn}>Hola</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,26 +40,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ff00ff'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ff00ff",
   },
-  title:{
-    fontSize: 24, color: "black"
+  title: {
+    fontSize: 24,
+    color: "black",
   },
-  image:{
-    width:200,
-    height:200, 
+  image: {
+    width: 200,
+    height: 200,
   },
-  btn:{
-    marginBottom:20,
+  btn: {
+    marginBottom: 20,
     color: "blue",
-    backgroundColor:"blue",
-    padding:7,
-    margin:30
+    backgroundColor: "blue",
+    padding: 7,
+    margin: 30,
   },
-  textBtn:{
-    fontSize:24,
-  }
+  textBtn: {
+    fontSize: 24,
+  },
 });
