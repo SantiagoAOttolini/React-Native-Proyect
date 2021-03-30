a4e17e48c1311b1a7dcc8372a109840ca4a5741d
santiago <santiago.ottolini97@hotmail.com>
2021-03-30 11:34:02 -0300

    React-Native: Button

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import imageDiamond from './assets/diaomond.png'
 
 export default function App() {
   return (
@@ -8,8 +9,19 @@ export default function App() {
       <Text style={styles.title}>Hello World</Text>
       <Image
        source={{uri:"https://picsum.photos/200/200"}}
+       /* source={imageDiamond} */
        style={styles.image}
        />
+       <Button
+       color="red"
+       title="Add"
+       onPress={() => Alert.alert("Hola")}
+       />
+       <TouchableOpacity
+       style={styles.btn}
+       >
+         <Text style={styles.textBtn}>Hola</Text>
+       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,6 +40,16 @@ const styles = StyleSheet.create({
   },
   image:{
     width:200,
-    height:200
+    height:200, 
+  },
+  btn:{
+    marginBottom:20,
+    color: "blue",
+    backgroundColor:"blue",
+    padding:7,
+    margin:30
+  },
+  textBtn:{
+    fontSize:24,
   }
 });
